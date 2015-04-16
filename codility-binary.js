f7d1f65be126c842aa181bde77a7cb1cb682cad5
Connor Leech <connorleech@gmail.com>
2015-04-15 20:08:20 -0700

    add incorrect binary exercise

--- codility-binary.js
+++ codility-binary.js
@@ -1,20 +1,43 @@
 function solution(M, N) {
-    var K = M.toString(2);
-    var L = N.toString(2);
-    var lenDiff = 0;
 
-    // zero padding
-    if(K.length > L.length){
-    	lenDiff = K.length - L.length;
-    	zeros = Array(lenDiff + 1).join('0');
-    	L = zeros + L;
-    } else {
-    	lenDiff = L.length - K.length;
-    	zeros = Array(lenDiff + 1).join('0');
-    	K = zeros + K;
+    var range = [];
+    while(M <= N){
+    	range.push(M);
+    	M++
     }
 
-    return L;
+    var zeroPad = function(bin, l){
+    	l = l - bin.length;
+    	return Array(l + 1).join('0') + bin;
+    };
+
+    var turnToBitString = function(intg){
+    	return intg.toString(2);
+    };
+
+    var maxLength = turnToBitString(range[range.length - 1]).length;
+
+    // make list of zero padded binary numbers with range M to N
+    range.forEach(function(e, i, list){
+    	list[i] = zeroPad(turnToBitString(e), maxLength);
+    });
+
+    // perform bitand
+    var bitand = function(a, b){
+    	var output = '';
+	    for(var i = 0; i < a.length; i++){
+	    	if(a[i] === '1' && b[i] === '1'){
+	    		// add one to answer
+	    		output += '1';
+	    	} else {
+	    		output += '0';
+	    	}
+	    }
+	    return output;
+	};
+
+	return parseInt(bitand(range[0], range[range.length - 1]), 2);
+
 }
 
-console.log(solution(21, 12))
+console.log(solution(5, 7))
